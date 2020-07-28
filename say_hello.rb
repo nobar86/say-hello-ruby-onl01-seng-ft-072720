dfee25fa86046a29d49f5853610c30790ccc9dcb
Michael Rabon <nobar86@gmail.com>
2020-07-28 18:29:54 +0000

    Automatically backed up by Learn

--- say_hello.rb
+++ say_hello.rb
@@ -2,6 +2,6 @@ def say_hello(name)
   puts "Hello Kent Beck!"
 end
 
-def say_hello(name = "Ruby Programmer")
-  puts "Hello Kent Beck!"
+def say_hello(name = "Kent Beck")
+  puts "Hello Ruby Programmer!"
 end
